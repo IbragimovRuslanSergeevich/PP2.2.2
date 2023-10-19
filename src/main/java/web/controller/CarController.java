85893a65aff540bc4683ec41c2f86e5513018128
IbragimovRuslanSergeevich <rona88hh@gmail.com>
2023-10-19 20:56:12 +0300

    Исправлено после замечаний

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -6,29 +6,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
+import web.service.CarServiceImpl;
 
 @Controller
 @RequestMapping("/cars")
 public class CarController {
-
-    private final CarService carService;
-
-    @Autowired
-    public CarController(CarService carService) {
-        this.carService = carService;
-    }
-
     @GetMapping()
-    public String showList(Model model) {
-        model.addAttribute("mycars", carService.getAllCars());
+    public String showCountList(@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
+        model.addAttribute("mycars", new CarServiceImpl().getCountCars(count));
         return "cars";
-    }
-
-    @GetMapping("?count={count}")
-    public String showCountList(@PathVariable("count") int count, Model model) {
-        model.addAttribute("mycars2", carService.getCountCars(count));
-        return "cars2";
     }
 
 
